a3264214b93f58538e99057670f01edf535a59ce
usman362 <usmanshoaib362@gmail.com>
2024-11-05 04:57:48 +0500

    npm run build

---
+++ public/build/assets/AuthPageheader-D744tn4h.js
@@ -0,0 +1,1 @@
+import{o as e,c as o,t as a}from"./app-DxJbJdXJ.js";const s={class:"py-3 px-2 text-slate-800 text-lg md:text-2xl font-medium font-['Roboto'] tracking-tight md:leading-loose bg-white"},c={__name:"AuthPageheader",props:{title:String},setup(t){return(i,n)=>(e(),o("div",s,a(t.title),1))}};export{c as _};
