f8d5f080e46c0da2fe27651515574c2b17a66807
usman362 <usmanshoaib362@gmail.com>
2024-11-14 00:57:37 +0500

    changes

--- resources/js/components/LoginModal.vue
+++ resources/js/components/LoginModal.vue
@@ -700,6 +700,7 @@ const loginWithFacebook = () => {
     signInWithPopup(auth, facebookProvider)
         .then(async (result) => {
             const user = result.user;
+            console.log('user',user);
             const idToken = await user.getIdToken();  // Get Firebase auth token
 
             // Send the token to your Laravel backend
